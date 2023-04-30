33bc21c21d2378ffc81f457700209a498ddd3e07
Diego Beraldin <diego.beraldin@gmail.com>
2023-04-30 21:35:46 +0200

    chore(persistence): tests for property values (#19)

---
+++ core-persistence/tests/src/jvmMain/kotlin/dao/TermPropertyValueDAOTest.kt
@@ -0,0 +1,129 @@
+package dao
+
+import AppDatabase
+import MockFileManager
+import data.EntryModel
+import data.LanguageModel
+import data.PropertyLevel
+import data.PropertyModel
+import data.PropertyValueModel
+import data.TermModel
+import data.TermbaseModel
+import kotlinx.coroutines.runBlocking
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class TermPropertyValueDAOTest {
+
+    private lateinit var appDb: AppDatabase
+    private lateinit var sut: TermPropertyValueDAO
+    private var propertyId: Int = 0
+    private var entryId: Int = 0
+    private var languageId: Int = 0
+    private var termId: Int = 0
+
+    @BeforeTest
+    fun setup() {
+        MockFileManager.setup()
+        appDb = AppDatabase(
+            filename = "test",
+            fileManager = MockFileManager
+        )
+        sut = appDb.termPropertyValueDao()
+
+        val termbaseDAO = appDb.termbaseDao()
+        val propertyDAO = appDb.propertyDao()
+        val entryDAO = appDb.entryDao()
+        val languageDAO = appDb.languageDao()
+        val termDAO = appDb.termDao()
+        runBlocking {
+            val termbaseId = termbaseDAO.create(TermbaseModel(name = "test"))
+            propertyId =
+                propertyDAO.create(
+                    PropertyModel(
+                        name = "test",
+                        termbaseId = termbaseId,
+                        level = PropertyLevel.LANGUAGE
+                    )
+                )
+            entryId = entryDAO.create(EntryModel(termbaseId = termbaseId))
+            languageId = languageDAO.create(LanguageModel(termbaseId = termbaseId, code = "en"))
+            termId = termDAO.create(TermModel(entryId = entryId, lemma = "test", lang = "en"))
+        }
+    }
+
+    @AfterTest
+    fun teardown() {
+        MockFileManager.teardown()
+    }
+
+    @Test
+    fun givenEmptyTermbaseWhenPropertyValueIsCreatedThenRowIsCreated() {
+        val model = PropertyValueModel(propertyId = propertyId, value = "test")
+        runBlocking {
+            val id = sut.create(model = model, termId = termId)
+            assert(id > 0)
+        }
+    }
+
+    @Test
+    fun givenExistingPropertyValueWhenGetByIdIsCalledThenValueIsReturned() {
+        val model = PropertyValueModel(propertyId = propertyId, value = "test")
+        runBlocking {
+            val id = sut.create(model = model, termId = termId)
+
+            val res = sut.getById(id)
+            assert(res != null)
+            assert(res?.value == "test")
+        }
+    }
+
+    @Test
+    fun givenExistingPropertyValuesWhenGetAllIsCalledThenValueIsReturned() {
+        val values = listOf(
+            PropertyValueModel(propertyId = propertyId, value = "test 1"),
+            PropertyValueModel(propertyId = propertyId, value = "test 2"),
+            PropertyValueModel(propertyId = propertyId, value = "test 3"),
+        )
+        runBlocking {
+            for (v in values) {
+                sut.create(model = v, termId = termId)
+            }
+
+            val res = sut.getAll(termId = termId)
+            assert(res.size == values.size)
+        }
+    }
+
+    @Test
+    fun givenExistingPropertyValueWhenIsDeletedThenNoValueIsReturned() {
+        val model = PropertyValueModel(propertyId = propertyId, value = "test")
+        runBlocking {
+            val id = sut.create(model = model, termId = termId)
+            val old = sut.getById(id)
+            assert(old != null)
+
+            sut.delete(model.copy(id = id))
+
+            val res = sut.getById(id)
+            assert(res == null)
+        }
+    }
+
+    @Test
+    fun givenExistingPropertyValueWhenIsUpdatedThenNoValueIsReturned() {
+        val model = PropertyValueModel(propertyId = propertyId, value = "test")
+        runBlocking {
+            val id = sut.create(model = model, termId = termId)
+            val old = sut.getById(id)
+            assert(old != null)
+
+            sut.update(model.copy(id = id, value = "test 2"))
+
+            val res = sut.getById(id)
+            assert(res != null)
+            assert(res?.value == "test 2")
+        }
+    }
+}
